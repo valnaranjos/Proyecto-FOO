33f57a19bbcc74f0d19d5b578575da021c74fa6a
Poryoculus <rodrigopachecojor@gmail.com>
2025-05-07 17:06:18 -0400

    importando el sextype a los archivos

--- src/ProyectoFoo.Application/Features/Patients/GetAllPatientsHandler.cs
+++ src/ProyectoFoo.Application/Features/Patients/GetAllPatientsHandler.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProyectoFoo.Application.Common.Enums;
+
 
 namespace ProyectoFoo.Application.Features.Patients
 {
