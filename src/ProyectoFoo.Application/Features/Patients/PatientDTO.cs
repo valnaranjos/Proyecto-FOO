21d8b7de1cace69c87ad5aef7d985d50d92b07fd
Poryoculus <rodrigopachecojor@gmail.com>
2025-05-08 22:14:36 -0400

    patient Age y RangoEtario

--- src/ProyectoFoo.Application/Features/Patients/PatientDTO.cs
+++ src/ProyectoFoo.Application/Features/Patients/PatientDTO.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ProyectoFoo.Application.Common.Enums;
+
 namespace ProyectoFoo.Application.Features.Patients
 {
-    public class PatientDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Surname { get; set; } = string.Empty;
-
-        public DateTime Birthdate { get; set; }
-        public int Identification { get; set; }
-        
-        
-        public SexType Sex { get; set; } = SexType.Masculino;
-        public string Modality { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string Phone { get; set; } = string.Empty;
-        // las propiedades que faltan (no obligatorias al crear, pero necesarias)
-    }
+    
+   public class PatientDTO
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Surname { get; set; } = string.Empty;
+    public DateTime Birthdate { get; set; }
+    public int Identification { get; set; }
+  
+    public SexType Sex { get; set; } = SexType.Masculino;
+    public string Modality { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+  
+    // Propiedades adicionales para respuesta
+    public int Age { get; set; }
+    public DateTimeOffset AdmissionDate { get; set; }
+    public string RangoEtario { get; set; } = string.Empty;
+  }
 }
--- src/ProyectoFoo.Domain/Entities/Paciente.cs
+++ src/ProyectoFoo.Domain/Entities/Paciente.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProyectoFoo.Domain.Entities
 {
@@ -13,48 +7,42 @@ namespace ProyectoFoo.Domain.Entities
     {
         public int Id { get; set; }
 
-       
+        [Required]
         public string Name { get; set; }
 
-        
+        [Required]
         public string Surname { get; set; }
 
-
+        [Required]
         public DateTime Birthdate { get; set; }
 
-        
+        [Required]
         public int Identification { get; set; }
 
-       
-        public string Sex { get; set; }
+        [Required]
+        public SexType Sex { get; set; }
 
-        
         public enum SexType
         {
-        Femenino,
-        Masculino,
-        Otro
+            Femenino,
+            Masculino,
+            Otro
         }
 
-       
-       
         public string? Email { get; set; }
-
-
 
         public string? Phone { get; set; }
 
-
+        [Required]
         public string Modality { get; set; }
 
-        //Se calcula automática
         public DateTimeOffset AdmissionDate { get; set; } = DateTime.UtcNow;
 
         public int Age { get; set; }
 
+        public string RangoEtario { get; set; } = string.Empty;
 
-
-        // // Constructor sin parámetros (requerido por EF Core) para instanciar las entidades al leer la base de datos.
+        // Constructor requerido por EF Core
         public Paciente()
         {
             Name = string.Empty;
@@ -63,36 +51,49 @@ namespace ProyectoFoo.Domain.Entities
             Modality = string.Empty;
         }
 
-        public Paciente( string name, string surname, DateTime birthdate, int identification, string sex, string modality, string email, string phone)
+        public Paciente(string name, string surname, DateTime birthdate, int identification, string sex, string modality, string email, string phone)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
             Surname = surname ?? throw new ArgumentNullException(nameof(surname));
             Birthdate = birthdate;
             Identification = identification;
-            Sex = sex ?? throw new ArgumentNullException(nameof(sex)); ;
+            Sex = sex ?? throw new ArgumentNullException(nameof(sex));
             Modality = modality ?? throw new ArgumentNullException(nameof(modality));
             Email = email;
             Phone = phone;
             AdmissionDate = DateTime.UtcNow;
-            Age = birthdate.Year - DateTime.UtcNow.Year;
+
+            CalcularEdadYRangoEtario();
         }
 
-        public Paciente(int id, string name, string surname, DateTime birthdate, int identification, string sex, string modality, string diagnosis,string email, string phone)
+        public Paciente(int id, string name, string surname, DateTime birthdate, int identification, string sex, string modality, string diagnosis, string email, string phone)
         {
             Id = id;
-            Name = name;
-            Surname = surname;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Surname = surname ?? throw new ArgumentNullException(nameof(surname));
             Birthdate = birthdate;
             Identification = identification;
-            Sex = sex;
-            Modality = modality;
-            Age = birthdate.Year - DateTime.UtcNow.Year;
-           
+            Sex = sex ?? throw new ArgumentNullException(nameof(sex));
+            Modality = modality ?? throw new ArgumentNullException(nameof(modality));
             Email = email;
             Phone = phone;
             AdmissionDate = DateTime.UtcNow;
+
+            CalcularEdadYRangoEtario();
         }
 
+        private void CalcularEdadYRangoEtario()
+        {
+            var today = DateTime.UtcNow;
+            Age = today.Year - Birthdate.Year;
+            if (Birthdate > today.AddYears(-Age)) Age--;
 
+            if (Age <= 12)
+                RangoEtario = "Niño";
+            else if (Age < 18)
+                RangoEtario = "Adolescente";
+            else
+                RangoEtario = "Adulto";
+        }
     }
 }
