9b3d68db3c74c62c295061d9738996aa391b6cf3
valnaranjos <ingvalerianaranjosep@gmail.com>
2025-05-12 13:28:42 -0500

    Migración para cambiar columnas Sex y Modality, para coherencia con la bd y los enums

---
+++ src/ProyectoFoo.Infrastructure/Migrations/20250512180308_ChangeEnumColumnsToInt.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFoo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeEnumColumnsToInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Cambiar la columna 'Sex' de varchar(1) a int para coherencia con el enum.
+            migrationBuilder.AlterColumn<int>(
+                name: "Sex",
+                table: "Pacientes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(1)");
+
+            // Cambiar la columna 'Modality' a int para coherencia con el enum.
+            migrationBuilder.AlterColumn<int>(
+                name: "Modality",
+                table: "Pacientes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revertir los cambios en caso de ser necesario.
+            migrationBuilder.AlterColumn<string>(
+                name: "Sex",
+                table: "Pacientes",
+                type: "varchar(1)",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modality",
+                table: "Pacientes",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
